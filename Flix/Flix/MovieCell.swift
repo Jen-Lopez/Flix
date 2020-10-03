4f6eacc59eb2269f1c3775560778ba7bb68a1708
Jen <lopzjcan@gmail.com>
2020-10-03 02:53:41 -0400

    implemented tab controller and cv

--- Flix/Flix/MovieCell.swift
+++ Flix/Flix/MovieCell.swift
@@ -14,6 +14,7 @@ class MovieCell: UITableViewCell {
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var synopsis: UILabel!
     
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
---
+++ Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jennifer Lopez on 10/3/20.
+//  Copyright © 2020 Jennifer Lopez. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var poster: UIImageView!
+}
